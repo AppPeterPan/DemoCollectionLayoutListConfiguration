fb9ea4535f5bff86af6f28ca06fdfb96891408d7
peter <apppeterpan@gmail.com>
2023-01-09 10:49:21 +0800

    Initial Commit

---
+++ DemoCollectionLayoutListConfiguration/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoCollectionLayoutListConfiguration
+//
+//  Created by Peter Pan on 2023/1/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
